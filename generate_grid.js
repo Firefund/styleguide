1d4cef2ef520e4dd354c1ff2dfe3b35944a4c90d
dotnetcarpenter <jon.ronnenberg@gmail.com>
2016-04-30 18:00:34 +0200

    fixing the mobile and last cell issue

--- generate_grid.js
+++ generate_grid.js
@@ -17,43 +17,84 @@
 	}
 	```
  */
-const grid = (columns) => {
+const grid = (cells) => {
 	const startBracket = " {\r\n"
 	const endBracket = "\r\n}\r\n"
 	const css = [
 		".f-row_",
-		columns,
+		cells,
 		" .f-row__cell_",
 		null,//n
 		startBracket,
 		"\tlost-column: ",
 		null,//n
 		"/",
-		columns,
+		cells,
 		" flex;",
 		endBracket,
 		".f-row_",
-		columns,
+		cells,
 		" .f-row__cell_push_",
 		null,//n
 		startBracket,
   	"\tlost-offset: -",
 		null,//n
 		"/",
-		columns,
+		cells,
 		";",
 		endBracket
 	]
 	const csss = []
-	for (let n = 1; n < columns; n++) {
+	for (let n = 1; n < cells; n++) {
+		csss.push(css.map(a => a == null ? n : a).join(""))
+	}
+	csss.push("\r\n")
+	return csss.join("")
+}
+const grid_mobile = (cells) => {
+	const startBracket = " {\r\n"
+	const endBracket = "\r\n}\r\n"
+	const css = [
+		".f-row_",
+		cells,
+		" .f-row__cell_",
+		null,//n
+		startBracket,
+		"\tlost-column: ",
+		null,//n
+		"/",
+		cells,
+		" flex;",
+		endBracket,
+		".f-row_",
+		cells,
+		" .f-row__cell_push_",
+		null,//n
+		startBracket,
+  	"\tlost-offset: -",
+		null,//n
+		"/",
+		cells,
+		";",
+		endBracket
+	]
+	const csss = []
+	for (let n = 1; n < cells; n++) {
 		csss.push(css.map(a => a == null ? n : a).join(""))
 	}
 	csss.push("\r\n")
 	return csss.join("")
 }
 
+const grid_6 = grid(6)
+const grid_12 = grid(12)
+const grid_24 = grid(24)
+const grid_mobile_6 = grid_mobile(6)
+const grid_mobile_12= grid_mobile(12)
+const grid_mobile_24= grid_mobile(24)
+
 console.log(`
-@lost gutter 3.6rem;
+@lost gutter 3.4rem;
 
 .f-row {
   lost-flex-container: row;
@@ -65,10 +106,28 @@ console.log(`
 .f-row__cell {
   lost-column: 1 flex;
 }
-`)
 
-console.log(
-	grid(6),
-	grid(12),
-	grid(24)
-)
+@media screen and (--viewport-desktop) {
+	.f-row__not-desktop { display: none; }
+
+	${grid_6}
+
+	${grid_12}
+
+	${grid_24}
+}
+
+
+/*
+ ## mobile
+*/
+@media screen and (--viewport-mobile) {
+  .f-row__not-mobile { display: none; }
+
+	${grid_mobile(6)}
+	
+	${grid_mobile(12)}
+	
+	${grid_mobile(24)}
+}
+`)
