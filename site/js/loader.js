27156ee90c155d65e7fe9160d727a8ed6499b19f
dotnetcarpenter <jon.ronnenberg@gmail.com>
2016-05-11 21:53:47 +0200

    first web component working

---
+++ site/js/loader.js
@@ -0,0 +1,35 @@
+﻿(function() {
+	var webComponentsSupported = ('registerElement' in document
+		&& 'import' in document.createElement('link')
+		&& 'content' in document.createElement('template'))
+
+	if (!webComponentsSupported) {
+		var wcPoly = document.createElement('script')
+		wcPoly.src = '../lib/webcomponents.js' // TODO: change to webcomponents-lite.min.js
+		wcPoly.onload = lazyLoadPolymerAndElements
+		document.head.appendChild(wcPoly)
+	} else {
+		lazyLoadPolymerAndElements()
+	}
+
+	function lazyLoadPolymerAndElements () {
+		console.log("Set up the element imports")
+		// Set up the element imports.
+		window.Polymer = window.Polymer || {}
+		window.Polymer.dom = 'shadow'
+
+		var elements = [
+			'/wc/header.htm'
+		]
+
+		elements.forEach(function(elementURL) {
+
+			var elImport = document.createElement('link')
+			elImport.rel = 'import'
+			elImport.href = elementURL
+
+			document.head.appendChild(elImport)
+
+		})
+	}
+}())
